c893f5e47d48ce7c59fe5aca0e6798aa6b574f50
travischamness <A3145629>
2018-10-31 14:47:16 -0700

    10/31

--- include/binarysearch.h
+++ include/binarysearch.h
@@ -1,5 +1,4 @@
 #include <cstdint>
-
 #include<iostream>
 
 
@@ -12,8 +11,8 @@ namespace edu {
 				template<class T>
 
 				int64_t binarySearch(const T &value, T array[], size_t size) {
-                    int64_t mid = 0, high = 0, low = 0;
-                    for (low = 0, high = size - 1; high > low; mid = (high - low) / 2) {
+                    int64_t mid, high = size - 1, low;
+                    for (low = 0; high >= low; mid = (high - low)/2 ) {
 
                         if (array[mid] == value) {
                             return mid;
@@ -27,35 +26,7 @@ namespace edu {
                         return -1;
 
 
-
-
-
-
                     }
-
-				    /*
-				     * --------Previous
-				     *
-					if (size == 0) return -1;
-					int64_t low = 0;
-					int64_t high = size - 1;
-					int64_t mid = (high + low) / 2;
-					
-					if (array[mid] == value) {
-						return mid;
-					}
-					else while (high >= low) {
-							if (array[mid] > value) {
-								high = mid - 1;
-								return -1;
-							}
-							if (array[mid] < value) {
-								low = mid + 1;
-								return -1;
-
-							}
-							return -1; */
-						}
 				}
 			}
 		}
